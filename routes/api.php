fe74d382725a11773ea7921bf1a1ee7894c4a1e2
anasfalah3 <anasfalah3@gmail.com>
2024-08-03 17:17:13 +0100

    api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\EmailVerificationController;
+use App\Http\Controllers\PasswordResetController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('/email-verification', [EmailVerificationController::class, 'sendVerificationEmail']);
+Route::get('/email-verification/{token}', [EmailVerificationController::class, 'verifyEmail']);
+
+Route::post('/password_reset', [PasswordResetController::class, 'sendPasswordResetEmail']);
+Route::get('/password_reset/{token}', [PasswordResetController::class, 'PasswordResetEmail']);
